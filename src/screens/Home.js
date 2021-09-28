062f4356364e3bc530a1026d60b30a9a361ba19d
구하민 <hamin2065@gmail.com>
2021-09-28 16:47:06 +0900

    Like Photos & Fragment

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,10 +1,34 @@
-import { logUserOut } from "../apollo";
+import { gql, useQuery } from "@apollo/client";
+import Photo from "../components/feed/Photo";
+import PageTitle from "../components/PageTitle";
+
+const FEED_QUERY = gql`
+  query seeFeed {
+    seeFeed {
+      id
+      user {
+        username
+        avatar
+      }
+      file
+      caption
+      likes
+      comments
+      createdAt
+      isMine
+      isLiked
+    }
+  }
+`;
 
 function Home() {
+  const { data } = useQuery(FEED_QUERY);
   return (
     <div>
-      <hi>Home</hi>
-      <button onClick={() => logUserOut()}>Log Out!</button>
+      <PageTitle title="Home" />
+      {data?.seeFeed?.map((photo) => (
+        <Photo key={photo.id} {...photo} />
+      ))}
     </div>
   );
 }
